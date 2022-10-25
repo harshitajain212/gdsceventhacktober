4c23f7e7460ed8308984b7e09d7bfc0ff34549af
Harshita jain <105262269+harshitajain212@users.noreply.github.com>
2022-10-25 21:05:55 +0530

    Create Harshita_5.py
    Program to check whether the number is odd or even.

---
+++ Harshita_5.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 20 16:30:47 2019
+
+@author: PC
+"""
+
+x=int(input('enter a number'))
+if x%2==0:
+    print('number is even')
+else:
+    print('number is odd')
+    
